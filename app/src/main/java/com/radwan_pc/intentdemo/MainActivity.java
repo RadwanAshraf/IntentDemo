06a2f1fe815bea20301c50b1d3037fb669bfc9e5
RadwanFCIS <radwan.ashraf.fcis@gmail.com>
2018-01-29 11:23:53 +0200

    update 1

--- app/src/main/java/com/radwan_pc/intentdemo/MainActivity.java
+++ app/src/main/java/com/radwan_pc/intentdemo/MainActivity.java
@@ -1,13 +1,27 @@
 package com.radwan_pc.intentdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+        Button button;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        button=findViewById(R.id.B_SecActivity);
+        //start sec activity
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        }
 }
